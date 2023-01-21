472e047b84f053c091b549e6738112369d355e12
Clinelly <swall289@gmail.com>
2023-01-21 13:21:02 +0000

    Build student class and tests.

--- evens.py
+++ evens.py
@@ -3,7 +3,7 @@ def even_number_of_evens(numbers):
         evens = sum([1 for n in numbers if n % 2 == 0])
 
         return True if evens and evens % 2 == 0 else False
-  
+
     else:
         raise TypeError("A list was not passed into the function")
 
---
+++ students.py
@@ -0,0 +1,35 @@
+from datetime import date, timedelta
+import requests
+
+
+class Student:
+    """A student class for test purposes"""
+
+    def __init__(self, first_name, last_name):
+        self._first_name = first_name
+        self._last_name = last_name
+        self._start_date = date.today()
+        self.end_date = date.today() + timedelta(days=365)
+        self.naughty_list = False
+
+    @property
+    def full_name(self):
+        return f"{self._first_name} {self._last_name}"
+
+    @property
+    def email(self):
+        return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
+
+    def apply_extension(self, days):
+        self.end_date = self.end_date + timedelta(days=days)
+
+    def alert_santa(self):
+        self.naughty_list = True
+
+    def course_schedule(self):
+        response = requests.get(f"https://company.com/course-schedule/{self.last_name}/{self.first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong with the request."
